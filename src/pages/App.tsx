14ed178be02b563f048df4c96de8ce338743789b
misosiru0205 <misosiru0205@gmail.com>
2024-02-01 03:22:00 +0900

    git commit -u

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -8,7 +8,7 @@ const App:React.FC = () => {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/react-kiso4/" element={<Home/>}/>
     </Routes>
     </BrowserRouter>
     </>
