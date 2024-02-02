e8c1e3e783f4388e5d8a896a032b5395e96920f5
misosiru0205 <misosiru0205@gmail.com>
2024-02-02 09:51:01 +0900

    git commit -u

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import { Inputs } from "./Home";
 
-const Header: React.FC<Inputs> = (props) => {
-  const [response, setResponse] = useState<Inputs>();
+const Header:React.FC<Inputs> = (props) =>{
 
-  useEffect(() => {
-    setResponse(props);
-  }, [props]);
+    const [response,setResponse] = useState<Inputs>()
+    
+    useEffect(()=>{
+        setResponse(props)
+    },[props])
 
-  return (
-    <header>
-      <div className="header__container">
-        <h1 className="header__title">自己紹介</h1>
-        {response && <p className="header__username">{response.name}</p>}
-      </div>
-    </header>
-  );
-};
+    return(
+        <header >
+            <div className="header__container">
+            <h1 className="header__title">自己紹介</h1>
+            {response && <p className="header__username">{response.name}</p>}
+            </div>
+        </header>
+    )
+}
 
-export default Header;
+export default Header
