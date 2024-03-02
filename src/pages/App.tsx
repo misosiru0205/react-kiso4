261cd4668dc7031558038629b0655a57daf3afbd
misosiru0205 <misosiru0205@gmail.com>
2024-03-02 20:07:13 +0900

    issue test

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,13 +5,11 @@ import Home from "./Home";
 
 const App: React.FC = () => {
   return (
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/react-kiso4/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/react-kiso4/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
