16f910d3d7fee0084bcb09af695bea2a753191c4
misosiru0205 <misosiru0205@gmail.com>
2024-03-02 12:31:43 +0900

    dev test

--- webpack.config.js
+++ webpack.config.js
@@ -4,52 +4,17 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 //依存関係を解決するやつ
 const nodeExternals = require("webpack-node-externals")
 
-//クライアント側に渡すjsをバンドルする
-const client = {
-    target:["web"],
-    mode: "development",
-    entry: "./src/client.tsx",
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: "client.js"
-    },
-    devServer:{static:"dist",open:true},
-    module: {
-        rules: [
-           {
-             test:  /\.(jsx|js|ts|tsx)?$/,
-             exclude:/node_modules/,
-             use:{
-              loader:"babel-loader",
-             }
-          },
-          {
-            test:/\.(css|scss)?$/,
-            use: [{loader:MiniCssExtractPlugin.loader},{loader:"css-loader"},{loader:"sass-loader"}],
-            exclude: /node_modules/,
-          },
-         ]
-       },
-       resolve: {
-         extensions: [".ts", ".tsx", ".js"]
-       },
-       plugins: [
-        new MiniCssExtractPlugin({
-          filename:"client.css"
-        })
-    ]
-};
-
 //nodeの実行ファイルをバンドルする
 const server = {
+  //nodeで起動するのでtergetはnodeに
   target:["node"],
   mode: "development",
+  //entryはexpressを実装しているファイルに
   entry: "./server/index.js",
   output: {
       path: path.resolve(__dirname,'dist'),
       filename: "server.js"
   },
-  devServer:{static:"dist",open:true},
   externals:[nodeExternals()],
   module: {
       rules: [
@@ -65,6 +30,42 @@ const server = {
      resolve: {
        extensions: [".ts", ".tsx", ".js"]
      },
+};
+
+//クライアント側に渡すjsをバンドルする
+const client = {
+  //クライアント側で読ませるためにtargetはwebに
+  target:["web"],
+  mode: "development",
+  entry: "./src/client.tsx",
+  output: {
+      path: path.resolve(__dirname,'dist'),
+      filename: "client.js"
+  },
+  module: {
+      rules: [
+         {
+           test:  /\.(jsx|js|ts|tsx)?$/,
+           exclude:/node_modules/,
+           use:{
+            loader:"babel-loader",
+           }
+        },
+        {
+          test:/\.(css|scss)?$/,
+          use: [{loader:MiniCssExtractPlugin.loader},{loader:"css-loader"},{loader:"sass-loader"}],
+          exclude: /node_modules/,
+        },
+       ]
+     },
+     resolve: {
+       extensions: [".ts", ".tsx", ".js"]
+     },
+     plugins: [
+      new MiniCssExtractPlugin({
+        filename:"client.css"
+      })
+  ]
 };
 
 //webpackの設定の送信
